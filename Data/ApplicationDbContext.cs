805a3a5bb74ab770920b8acb1cd5429aa2de2743
Claudio Savoldi <savoldiclaudio02@gmail.com>
2026-01-05 16:16:15 +0100

    SetUp progetto

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ClinicaVeterinaria.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClinicaVeterinaria.Data
+{
+    public class ApplicationDbContext:IdentityDbContext<IdentityUser,IdentityRole,string>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<AnagraficaAnimale> AnagraficheAnimali { get; set; }
+        public DbSet<Visita> Visite { get; set; }
+        public DbSet<ProdottoMedicinale> ProdottiMedicinali { get; set; }
+        public DbSet<ProdottoAlimentare> ProdottiALimentari{ get; set; }
+        public DbSet<Proprietario> Proprietari { get; set; }
+        public DbSet<Ricovero> Ricoveri { get; set; }
+    }
+}
--- Models/Entities/ProdottoMedicinale.cs
+++ Models/Entities/ProdottoMedicinale.cs
@@ -2,10 +2,11 @@
 {
     public class ProdottoMedicinale
     {
-
         public Guid ProdottoMedicinaleId { get; set; }
         public string Nome { get; set; }
         public string DittaFornitrice { get; set; }
         public List<string> ElencoUsi { get; set; }
+
+        //Gestione armadietti da decidere!!!!!
     }
 }
